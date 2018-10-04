b26cf5bbd3ae23946dfb48826be2415c9f1d84e2
lilyisadreamer <ophelie.donizetti@outlook.fr>
2018-10-04 16:36:14 +0200

    maj

--- tutonav.php
+++ tutonav.php
@@ -1,21 +1,33 @@
 
 <?php include ("header1.html"); ?>
 
-   
-    <section>
-        <h2>
-        < NAV >
-        </h2>
-        <p>
-        La balise nav est une balise semantique sert à créer généralement une barre de menu, avec des liens interne et externe. 
-       Cette balise sert à indiquer une liste de liens dédiée à la navigation du site. Plusieurs balises nav 
-        </p>
-   
-    </section>
+<article>
 
-    <footer>
-2018 Acces code school . An Jun, Alain, Bilel, Ophélie . mention légal .
-    </footer>
+    <div class="gris"></div> 
+<section>
+    <div class="hero"></div>
+    <div class="fond">
 
+<h2>
+La balise nav
+</h2>
+
+<p>
+La balise nav est une balise semantique sert à créer généralement une barre de menu, avec des liens interne et externe. 
+ Cette balise sert à indiquer une liste de liens dédiée à la navigation du site. Plusieurs balises < nav > peuvent co exister sur une même page web. 
+
+</p>
+<br>
+<img src="images/tutonav.jpg" id="responsive-image" width="500px"> 
+
+    </div>
+
+</section>
+    <div class="gris"></div>
+</article>
+
+<footer>
+<div>2018-Belfort</div> <div>Access code school</div> <div>Prénoms des codeurs : AnJum, Alain, Bilel, Ophélie</div> <div>mention légal</div>
+</footer>
 </body>
 </html>
