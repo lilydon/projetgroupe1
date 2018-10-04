487e071c48fd0bfff95b754a0d53024995b08ee2
anjum18 <anjumandleeb@gmail.com>
2018-10-04 15:00:06 +0200

    ok

---
+++ titreanjum.php
@@ -0,0 +1,50 @@
+<?php include ("header1.html"); ?>
+
+
+
+<article>
+<div class=gris></div> 
+<section>
+<div class=hero></div>
+<div class=fond>
+                <h1>
+                    Titre 
+                </h1>
+                
+                    <p>L'élément titre définit le titre du document (qui est affiché dans la barre de titre du navigateur ou dans l'onglet de la page). Cet élément ne peut contenir que du texte, les balises qu'il contiendrait seraient ignorées.Pourquoi définir des titres et des paragraphes ?
+                        <BR> Une nouvelle fois, rappelez vous que le HTML est un langage de balisage.
+                       <br>
+                         Le seul et unique rôle du HTML est de nous permettre de créer un document structuré en différenciant d’un point de vue sémantique les différents éléments d’une page.
+                         <br>
+                          Le but de tout ceci est que les navigateurs et les ordinateurs puissent comprendre la structure de notre document et son flux afin de l’afficher de la meilleure manière.
+                         
+                         L’un des effets secondaires d’une bonne utilisation du HTML en tant que langage de balisage est un meilleur référencement sur des moteurs de recherche comme Google par exemple.
+                         
+                         Il va donc être essentiel de différencier nos titres de nos paragraphes en HTML, ainsi que les parties de contenu plus ou moins importantes, etc.
+                     
+                     <br><br>
+                         Les titres en HTML
+                         Il existe six niveaux hiérarchiques de titres ("heading" en anglais) définis par les éléments h1, h2, h3, h4, h5 et h6. L'élément h1 représente un titre très important, h2 un titre important, h3 un titre d'importance moyenne, etc.
+                           
+                    </p> 
+                    <div class=image>
+                        <img src="titres-html.png" alt="coding de heading">
+                        <br>
+
+                        <img src="titres.png" alt="resultat de heading">
+                    </div>
+                         
+                    
+                        
+                
+            </section>
+            <div class=gris></div>
+        </article>
+        <footer>
+            <div>25/09/2018 à Belfort</div>  <div>Acces code school</div> <div>Prénoms des codeurs : AnJum, Alain, Bilel, Ophélie</div> <div>mention légal</div>
+        </footer>
+    </body>
+</html>
+
+
+        
