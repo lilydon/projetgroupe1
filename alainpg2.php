bcdf1d87d79db9daee8508aa5e44e5f55903bcd3
Alain Fougerolle <Waralain25555@hotmail.fr>
2018-10-04 16:40:21 +0200

    plop

---
+++ alainpg2.php
@@ -0,0 +1,22 @@
+<?php include ("header1.html"); ?>
+    
+        <article>
+          <div class=gris></div>    
+            <section>
+                <div class=hero></div>
+                <div class=fond>
+                    <h2>
+                      Le Header
+                    </h2>
+                    <p>
+                        Le header et une balise Html 5 de type bloc representant l'en tête de la page.<br>Contrairement à des éléments génériques comme span ou div qui ont un rôle totalement neutre, et ne servent qu'à regrouper d'autres éléments HTML pour leur affecter un style CSS commun, voire pour interagir avec eux via le DOM... Il ne s'agit donc pas de nouveaux éléments avec des noms génériques : c'est bien plus que ça !
+                    </p>
+                </div>
+            </section>
+            <div class=gris></div>
+        </article>
+        <footer>
+            <div>25/09/2018 à Belfort</div>  <div>Acces code school</div> <div>Prénoms des codeurs : AnJum, Alain, Bilel, Ophélie</div> <div>mention légal</div>
+        </footer>
+    </body>
+</html>
