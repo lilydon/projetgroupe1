0400eddf51d5feef2d516fe878f7d5f99043b7b5
anjum18 <anjumandleeb@gmail.com>
2018-10-04 15:08:07 +0200

    reload pgs

---
+++ anjumpg2.php
@@ -0,0 +1,39 @@
+          
+<?php include ("header1.html"); ?>
+
+
+
+<article>
+<div class=gris></div> 
+<section>
+<div class=hero></div>
+<div class=fond>
+<h2>
+Tutorial <strong> Balises </strong>
+</h2>
+<h2>link </h2>
+<p>
+    
+        Balise LINK
+        Rôle
+
+        La balise <link> définit la relation entre un document et une ressource externe. La balise <link> est surtout utilisée pour lier les feuilles de style CSS.
+       <br><br>
+
+       <h2>Attributs spécifiques à cet élément</h2><br>
+
+        <div class=image>
+                <img src="link.png" alt="examples de differents links">
+
+            </div>
+</p>
+</div>
+</section>
+<div class=gris></div>
+</article>
+<footer>
+<div>2018-Belfort</div> <div>Access code school</div> <div>Prénoms des codeurs : AnJum, Alain, Bilel, Ophélie</div> <div>mention légal</div>
+</footer>
+
+</body>
+</html>
---
+++ anjumpg3.php
@@ -0,0 +1,40 @@
+
+<?php include ("header1.html"); ?>
+
+
+
+<article>
+<div class=gris></div> 
+<section>
+<div class=hero></div>
+<div class=fond>
+    
+                <h2>
+                    Titre 
+                </h2>
+                
+                    <p> La balise <body> définit le corps du document.
+
+                            L’élément <body> contient tout le contenu d’un document HTML, comme le texte, des hyperliens, des images, des tableaux, des listes, etc.
+                    </p> 
+                    <div class=image>
+                        <img src="body1.png" alt="balise body">
+                        <br>
+
+                        
+                    </div>
+                         
+                    
+                        
+                
+            </section>
+            <div class=gris></div>
+        </article>
+        <footer>
+            <div>25/09/2018 à Belfort</div>  <div>Acces code school</div> <div>Prénoms des codeurs : AnJum, Alain, Bilel, Ophélie</div> <div>mention légal</div>
+        </footer>
+    </body>
+</html>
+
+
+       
