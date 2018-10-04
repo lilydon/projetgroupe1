d899f6265079cc6737b262e7f89637f4ca3745d7
Krez90 <bilel.hamimed@yahoo.fr>
2018-10-04 15:14:32 +0200

    maj3

---
+++ BibiForm.php
@@ -0,0 +1,50 @@
+<?php include ("header1.html"); ?>
+<article>
+    <div class=gris></div>
+    <section>
+        <div class=hero></div>
+        <div class=fond>
+            <h2>
+                La Balise FORM
+            </h2>
+            <p>
+                Présentation de la balise FORM
+                <br>
+                La balise form déclare une zone de formulaire dans laquelle les utilisateurs peuvent saisir des
+                informations qui pourront par la suite être récupérées et traitées côté serveur. Il est possible
+                d'utiliser plusieurs formulaires dans une même page sous condition de ne pas les imbriquer. Finalement,
+                pour que le formulaire puisse être envoyé il faut qu'il soit soumis,
+                soit à l'aide de la balise input : input type="submit" ... /
+                soit à l'aide du javascript en appliquant la fonction "submit()" au formulaire (en se basant sur son
+                identifiant)
+
+
+                D'autre part, pour utiliser cette notion de formulaire, il est important de donner les valeurs
+                adéquates aux principaux attributs de la balise "form":
+                <ul>
+                    <li> name : nom du formulaire utilisé pour faire référence à celui-ci.</li>
+                    <li> action : indique l'adresse d'envoi des données du formulaire lors de sa soumission.</li>
+                    <li> method : indique comment seront transmises les données au serveur ("get" ou "post").</li>
+                </ul>
+                Les éléments utilisés à l'intérieur d'un formulaire pour échanger explicitement des
+                informations
+                sont les suivant :
+                <ul>
+                    <li>button</li>
+                    <li>input avec les différents types qui lui sont associés (submit, text, checkbox ...)</li>
+                    <li>select</li>
+                    <li>textarea</li>
+                </ul>
+            </p>
+            <img src="images/elementForm.jpg" />
+
+    </section>
+    <div class=gris></div>
+</article>
+<footer>
+    2018 Acces code school . An Jun, Alain, Bilel, Ophélie . mention légal .
+</footer>
+
+</body>
+
+</html>
---
+++ BibiH1H6.php
@@ -0,0 +1,43 @@
+<?php include ("header1.html"); ?>
+<article>
+    <div class=gris></div>
+    <section>
+        <div class=hero></div>
+        <div class=fond>
+            <h2>
+                Balise (h1 à h6)
+            </h2>
+            <p>
+                Présentation de la balise h1 à h6
+                <br>
+                La balise h1 est une balise présente dans le code d’une page web pour marquer le titre principal du
+                contenu
+                (par exemple, le titre d’un article de blog, le titre principal du texte de la page…). A ne pas
+                confondre
+                avec
+                la balise title.
+                Les balises h2, h3… servent à marquer les « sous-titres » (h2), les « sous-sous-titres » (h3)…
+                Une seule balise h1 est prise en compte sur chaque page (il ne peut y avoir qu’un seul « grand titre »
+                à un
+                article de blog). En revanche, il peut y avoir plusieurs sous-titres (donc plusieurs balises h2, h3,
+                h4…
+                jusqu’à h6).
+                Ces balises sont généralement placées sur des mots (le titre et les sous-titres du texte de la page).
+                Mais
+                elles peuvent aussi être placées sur une image. On voit parfois des balises h1 placées sur le logo (ce
+                qui
+                est
+                une erreur pour le référencement).
+            </p>
+            <img src="images/Baliseh1.png" />
+
+    </section>
+    <div class=gris></div>
+</article>
+<footer>
+    2018 Acces code school . An Jun, Alain, Bilel, Ophélie . mention légal .
+</footer>
+
+</body>
+
+</html>
