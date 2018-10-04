32440fcb54d297d81d97af1d501739df787def33
lilyisadreamer <ophelie.donizetti@outlook.fr>
2018-10-04 13:32:02 +0200

    ophélie

--- projetgroupe.php
+++ projetgroupe.php
@@ -22,5 +22,4 @@ Les balises vous sont si abstraites que du Picasso ? Ce site est fait pour vous.
 <footer>
 <div>2018-Belfort</div> <div>Access code school</div> <div>Prénoms des codeurs : AnJum, Alain, Bilel, Ophélie</div> <div>mention légal</div>
 </footer>
-</body>
-</html>
+
