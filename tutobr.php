e9f39863ea4ff7abaf152654828db2c61830eec2
lilyisadreamer <ophelie.donizetti@outlook.fr>
2018-10-05 10:07:56 +0200

    mise a jour

--- tutobr.php
+++ tutobr.php
@@ -1,16 +1,24 @@
 
-<?.php include ("header1.html"); ?>
+<?php include ("header1.html"); ?>
    
-    <section>
+<article>
+
+    <div class="gris"></div> 
+<section>
+    <div class="hero"></div>
+    <div class="fond">
+
         <h2>
         BR 
         </h2>
         <p>
         l'élement < BR > sert à sauté à la ligne. il est principalement utilisé pour des poèmes ... l'usage doit rester modérer. Pour la mise en page, il y a margin a utilisé ...
     
-    <img scr="tutobr.jpg">
+    <img src="images/tutobr.jpg" id="responsive-image" width="500px">
 
     </section>
+    <div class="gris"></div>
+</article>
 
  <footer>
 2018 Acces code school . Anjum, Alain, Bilel, Ophélie . mention légal .
--- tutopre.php
+++ tutopre.php
@@ -1,3 +1,33 @@
 
-<?.php include ("header1.html"); ?>
+<?php include ("header1.html"); ?>
 
+<article>
+
+    <div class="gris"></div> 
+<section>
+    <div class="hero"></div>
+    <div class="fond">
+
+<h2>
+La balise PRE
+</h2>
+
+<p>
+    Elle permet de définir du texte préformaté. Le texte est affiché avec une police à largeur fixe, et cette balise préserve les espaces et les sauts de ligne.
+Cette balise est utilisé dans les mises en page spécifique et dans le code informatique.
+
+</p>
+<br>
+<img src="images/tutopre.jpg" id="responsive-image" width="500px"> 
+
+    </div>
+
+</section>
+    <div class="gris"></div>
+</article>
+
+<footer>
+<div>2018-Belfort</div> <div>Access code school</div> <div>Prénoms des codeurs : AnJum, Alain, Bilel, Ophélie</div> <div>mention légal</div>
+</footer>
+</body>
+</html>
