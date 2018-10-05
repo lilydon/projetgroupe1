86157c147e03947c5676d9a176ea85bf5b329099
anjum18 <anjumandleeb@gmail.com>
2018-10-05 11:59:05 +0200

    k

---
+++ index.php
@@ -0,0 +1,27 @@
+
+
+<?php include ("header1.php"); ?>
+
+
+
+<article>
+<div class=gris></div> 
+<section>
+<div class=hero></div>
+<div class=fond>
+<h2>
+Tutorial <strong> Balises </strong>
+</h2>
+<p>
+Les balises vous sont si abstraites que du Picasso ? Ce site est fait pour vous.<br> Vous trouverez des <strong>tutoriels</strong> sur ces petites créatures.<br> Les balises <strong>HTML</strong> n'aura plus de secret pour vous !
+</p>
+</div>
+</section>
+<div class=gris></div>
+</article>
+<footer>
+<div>2018-Belfort</div> <div>Access code school</div> <div>Prénoms des codeurs : AnJum, Alain, Bilel, Ophélie</div> <div>mention légal</div>
+</footer>
+   
+</body>
+</html>
