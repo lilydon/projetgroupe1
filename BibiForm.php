49b240e114800985d5190773750442f90a7a5de5
Krez90 <bilel.hamimed@yahoo.fr>
2018-10-05 10:24:54 +0200

    majbibi5

--- BibiForm.php
+++ BibiForm.php
@@ -7,18 +7,18 @@
             <h2>
                 La Balise FORM
             </h2>
-            <p>
-                Présentation de la balise FORM
+          
+                <h2>Présentation de la balise FORM</h2>
                 <br>
-                La balise form déclare une zone de formulaire dans laquelle les utilisateurs peuvent saisir des
+                <p>
+                La balise "form" déclare une zone de formulaire dans laquelle les utilisateurs peuvent saisir des
                 informations qui pourront par la suite être récupérées et traitées côté serveur. Il est possible
                 d'utiliser plusieurs formulaires dans une même page sous condition de ne pas les imbriquer. Finalement,
                 pour que le formulaire puisse être envoyé il faut qu'il soit soumis,
                 soit à l'aide de la balise input : input type="submit" ... /
                 soit à l'aide du javascript en appliquant la fonction "submit()" au formulaire (en se basant sur son
-                identifiant)
-
-
+                identifiant).
+                <br>
                 D'autre part, pour utiliser cette notion de formulaire, il est important de donner les valeurs
                 adéquates aux principaux attributs de la balise "form":
                 <ul>
@@ -26,7 +26,7 @@
                     <li> action : indique l'adresse d'envoi des données du formulaire lors de sa soumission.</li>
                     <li> method : indique comment seront transmises les données au serveur ("get" ou "post").</li>
                 </ul>
-                Les éléments utilisés à l'intérieur d'un formulaire pour échanger explicitement des
+                <p>Les éléments utilisés à l'intérieur d'un formulaire pour échanger explicitement des
                 informations
                 sont les suivant :
                 <ul>
